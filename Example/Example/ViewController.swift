9ab85e1dbb9ecfb18cce3d5393651dec0e0215d7
Fabijan Bajo <bajo09@gmail.com>
2017-04-17 17:10:15 +0200

    Extend the example with a storyboard example: embed a progress view inside a scrollview and track content offset changes

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -9,6 +9,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 import ContourProgressView
 
 class ViewController: UIViewController {
@@ -17,16 +19,37 @@ class ViewController: UIViewController {
     // MARK: - Properties
     
     fileprivate var rectProgressView: ContourProgressView!
+    fileprivate var trailerLocation: URL {
+        let documentsDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        return documentsDir.appendingPathComponent("trailer.mp4")
+    }
     private lazy var session: URLSession = {
         let configuration = URLSessionConfiguration.default
         return URLSession(configuration: configuration, delegate: self, delegateQueue: .main)
     }()
     
     
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var downloadLabel: UILabel!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var ovalProgressView: ContourProgressView!
+    
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
+    
+    
+    // MARK: - Methods
+    
+    private func configure() {
+        // ScrollView
+        scrollView.delegate = self
         
         // Progress
         rectProgressView = ContourProgressView(frame: view.bounds, isRect: true)
@@ -35,21 +58,31 @@ class ViewController: UIViewController {
         rectProgressView.progressTintColor = .black
         rectProgressView.lineWidth = 4
         view.addSubview(rectProgressView)
-        
+    }
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func downloadButtonPressed(_ sender: UIButton) {
         // A quick download example demonstrating progress & bytes written
         session.downloadTask(with: URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")!).resume()
     }
     
     
-    // MARK: - Methods
-    
-    
-    
-    // MARK: - Actions
-    
-    
-    
     // MARK: - Navigation
+}
+
+
+// MARK: - UIScrollViewDelegate
+
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        // ContourProgressView progress does not go beyond 1.0
+        let maxOffset: CGFloat = -150
+        let relativeProgress = CGFloat(scrollView.contentOffset.y / maxOffset)
+        rectProgressView.progress = relativeProgress
+        ovalProgressView.progress = relativeProgress
+    }
 }
 
 
@@ -61,6 +94,8 @@ extension ViewController: URLSessionDownloadDelegate {
         // Session is configured to shedule delegate calls on the main thread
         let currentProgress = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
         rectProgressView.progress = currentProgress
+        ovalProgressView.progress = currentProgress
+        downloadLabel.text = "\(Int(currentProgress * 100)) %"
     }
     
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
